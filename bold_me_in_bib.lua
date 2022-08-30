857c751f1760e34f76eaf9fcc59505fc62a45e22
Yuxiang Wei <yuxiang630.wei@gmail.com>
2022-08-30 11:05:26 -0500

    Fix bib

--- bold_me_in_bib.lua
+++ bold_me_in_bib.lua
@@ -4,11 +4,11 @@ local highlight_author_filter = {
         if el.t == "Para" then
             for k, _ in ipairs(el.content) do
                 -- print(el.content[k].text)
-                if el.content[k].t == "Str" and el.content[k].text == "Wei," and el.content[k + 1].t == "Space" and
-                    el.content[k + 2].t == "Str" and el.content[k + 2].text:find("Yuxiang") then
-                    local _, e = el.content[k + 2].text:find("Yuxiang")
+                if el.content[k].t == "Str" and el.content[k].text == "Yuxiang" and el.content[k + 1].t == "Space" and
+                    el.content[k + 2].t == "Str" and el.content[k + 2].text:find("Wei") then
+                    local _, e = el.content[k + 2].text:find("Wei")
                     local rest = el.content[k + 2].text:sub(e + 1)
-                    el.content[k] = pandoc.Strong {pandoc.Str("Wei, Yuxiang")}
+                    el.content[k] = pandoc.Strong {pandoc.Str("Yuxiang Wei")}
                     el.content[k + 1] = pandoc.Str(rest)
                     table.remove(el.content, k + 2)
                 end
